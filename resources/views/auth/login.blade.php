ee48db6cde1621d45af6c34df068f729899ae597
mdandyc <dandyforze@gmail.com>
2017-06-01 16:59:42 +0700

    add blade template for login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,68 +1,99 @@
-@extends('layouts.app')
 
-@section('content')
-<div class="container">
+
+
+
+<!DOCTYPE html>
+  <html>
+  <link href="/css/app.css" rel="stylesheet">
+      <link rel="shortcut icon" href="/img/icon.png">
+       <link type="text/css" rel="stylesheet" href="/css/materialize/materialize.min.css" >
+       <link rel="stylesheet" type="text/css" href="/css/materialize/style2.css">
+       <link href="/css/materialize/iconmaterialize.css" rel="stylesheet">
+      <title>Login</title>
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+
+    <body>
+      <nav>
+    <div class="nav-wrapper">
+      <a href="#" data-activates="mobile-demo" class="button-collapse">
+      <i class="material-icons">menu</i></a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a class="menunav" href="index.html">HOME</a></li>
+        <li><a class="menunav" href="register.html">REGISTER</a></li>
+      </ul>
+      <ul class="side-nav" id="mobile-demo">
+        <li><a class="waves-effect waves-light btn" href="index">Home</a></li>
+        <li><a class="waves-effect waves-light btn" href="register">Register</a></li>
+      </ul>
+    </div>
+  </nav>
+  <div id="test1" class="col s12">
+  <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
+      <div class="col s12">
+        <div class="login">
+         <div class="card-panel blue-grey lighten-5 z-depth-5">
+         <p class="center login-form-text">Untuk Meminjam Buku <br> Anda Harus Login Terlebih Dahulu</p>
+         <img class="gambar" src="img/logo.png">
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
+         <form action="{{ route('login') }}" method="POST">
+          {{ csrf_field() }}
+          
+              <div class="input-field col s12">
+              <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+              <i class="material-icons prefix">perm_identity</i>
+              <input id="icon_prefix email" type="email" class="validate form-control" name="email" value="{{ old('email') }}" required autofocus>
+              <label for="icon_prefix" class="control-label">E-Mail</label>
+               @if ($errors->has('email'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
-                                @endif
-                            </div>
-                        </div>
+               @endif
+              </div><!--end inputfield-->
+            </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
+              <div class="input-field col s12">
+              <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+              <i class="material-icons prefix">lock</i>
+              <input id="password" type="password" class="validate form-control" name="password" required>
+              <label for="password" class="control-label">Password</label>
+               @if ($errors->has('password'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
                                 @endif
-                            </div>
-                        </div>
+              </div><!--end inputfield-->
+              </div>
+              <p>
+              <input type="checkbox" name="checkbox" value="check" id="test5" />
+              <label for="test5">Saya Bersedia Bertanggung Jawab</label>
+              </p>
+              <div class="log">
+              <button class="btn waves-effect waves-light" type="submit" name="action" onclick="if(!this.form.checkbox.checked){alert('You must agree to the terms first.');return false}"><i class="material-icons left">done_all</i>Login
+            </button><!--end button-->
+              </div>
+        </form><!--end form-->
+        </div><!--end cardpanel-->
+        </div><!--end login-->  
+        </div><!--end cols12-->
+      </div><!--end rows-->
+    </div><!--end container-->
+  
+  </div>
+  
+        
+          
+      <!--Import jQuery before materialize.js-->
+   <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/materialize/materialize.min.js"></script>
+    <script>
+      $( document ).ready(function(){
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+        $(".button-collapse").sideNav();
+        
+      })
+    </script>
+    </body>
+  </html>
